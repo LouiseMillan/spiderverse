a4c3a10912fd625d0a5051541dfe948b8d607dad
LouiseMillan <milouisedu@gmail.com>
2022-05-07 13:12:38 -0500

    Step 2. Test OK

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,3 +1,5 @@
+const Spiderman = require('../app/spiderman');
+
 describe("Unit Tests for Spiderman class", () => {
     test("1) Create an Spiderman Object", () => {
         const amdrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony");
@@ -5,7 +7,7 @@ describe("Unit Tests for Spiderman class", () => {
         expect(amdrewGarfield.name).toBe("Spiderman Sony");
         expect(amdrewGarfield.age).toBe(31);
         expect(amdrewGarfield.actor).toBe("Andrew Garfield");
-        expect(amdrewGarfield.movies).toBe(31);
+        expect(amdrewGarfield.movies).toBe(2);
         expect(amdrewGarfield.studio).toBe("Sony");
     });
 });
