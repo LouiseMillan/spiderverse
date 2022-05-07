ec344b735ad39e41bf2a5cbd01df2a27807b4914
LouiseMillan <milouisedu@gmail.com>
2022-05-07 13:07:37 -0500

    Step 1. Write a test for the next bit of functionality you want to add

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,11 @@
-describe("Test Suite Dummy Description", () => {
-    TextDecoderStream("Case 1 Dummy", () => {
-        const resultOfSomething = 1 + 2;
-        extect(resultOfSomething).toBe(18);
+describe("Unit Tests for Spiderman class", () => {
+    test("1) Create an Spiderman Object", () => {
+        const amdrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony");
+
+        expect(amdrewGarfield.name).toBe("Spiderman Sony");
+        expect(amdrewGarfield.age).toBe(31);
+        expect(amdrewGarfield.actor).toBe("Andrew Garfield");
+        expect(amdrewGarfield.movies).toBe(31);
+        expect(amdrewGarfield.studio).toBe("Sony");
     });
 });
